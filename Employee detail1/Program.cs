ee15eeb4e959642e6511d4a7e160ea2311fb3a52
maverick9453 <amankumar9453@gmail.com>
2024-04-14 14:53:00 +0530

    Add project files.

---
+++ Employee detail1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_detail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the employee name Id ");
+            int employeeId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the employee name ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("enetr the employee salary ");
+            double salary = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
